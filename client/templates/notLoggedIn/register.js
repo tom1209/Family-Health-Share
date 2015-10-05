a4d24be8eeac4c31fdd6e9c9681ae2d0fc1f297d
tom1209 <medic@Medics-MacBook-Pro.local>
2015-10-05 11:11:13 -0400

    redid login and register pages, but I will need to create the database before I can go any further with this, I think

--- client/templates/notLoggedIn/register.js
+++ client/templates/notLoggedIn/register.js
@@ -2,3 +2,11 @@
  * Created by tom on 2015-10-04.
  * Will be used when a new user wants to register an account
  */
+
+Template.register.events({
+    'submit form':function(e){
+        e.preventDefault();
+        console.log("register firing!");
+        //Registration logic will go here
+    }
+});
