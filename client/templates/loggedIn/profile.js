8cefa821ba9baa69d50bc1c600754897cb567b11
tom1209 <tomwhite1209@gmail.com>
2015-10-17 13:30:34 -0400

    Working on adding health conditions to user profile

--- client/templates/loggedIn/profile.js
+++ client/templates/loggedIn/profile.js
@@ -3,3 +3,16 @@
  */
 
 //This is for the add Health Conditions button click event
+Session.set('showHealthConditionText', false);
+
+Template.profile.events({
+    'click #addHealthCondition': function(){
+        Session.set('showHealthConditionText', true);
+    }
+});
+
+Template.profile.helpers({
+    'showHealthConditionText' : function() {
+     return Session.get('showHealthConditionText');
+     }
+});
