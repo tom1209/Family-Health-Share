9b56f30ad94e6be1c22e686ce46afcf4b1f5d4a0
tom1209 <tomwhite1209@gmail.com>
2015-11-10 13:22:22 -0500

    Family page successfully refreshes if user hasFamily

--- client/templates/loggedIn/family/family.js
+++ client/templates/loggedIn/family/family.js
@@ -3,16 +3,34 @@
  * Javascript file for family page
  */
 
-var user = Meteor.userId();
-var hasFamily = user.profile.hasFamily;
-if (hasFamily == true)
-    Session.set('hasFamily', true);
-else
-    Session.set('hasFamily', false);
+Session.set('isFamilySet', false);
+
+//When the page renders
+Template.family.rendered = function(){
+
+    //The reason I have to put everything in the autorun function is because the page is rendering before the user accounts
+    //This means when I check the hasFamily property it returns undefined.
+    this.autorun(function(e) {
+        var user = Meteor.user();
+        var userHasFamily = user && user.profile.hasFamily;
+
+        if(userHasFamily)
+        {
+            if (userHasFamily === true)
+            {
+                Session.set('isFamilySet', true);
+            }
+        }
+    });
+
+
+}
+
 
 Template.family.helpers({
     //Determine whether a user has registered with a family or not
-    'hasFamily': function() {
-        return Session.get('hasFamily');
+    'isFamilySet': function() {
+        return Session.get('isFamilySet');
     }
 });
+
